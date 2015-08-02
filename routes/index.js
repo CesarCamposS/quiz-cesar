4af0deb085e2adfe190bd4c5da3945383388e53f
CesarCamposS <cesarcampos.1@gmail.com>
2015-08-02 17:40:02 +0200

    Añadir preguntas

--- routes/index.js
+++ routes/index.js
@@ -15,11 +15,11 @@ router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
 });
 
+// Definición de rutas de /quizes
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
-
-//router.get('/quizes/question', quizController.question);
-//router.get('/quizes/answer', quizController.answer);
+router.get('/quizes/new',		    quizController.new);
+router.post('/quizes/create',		    quizController.create);
 
 module.exports = router;

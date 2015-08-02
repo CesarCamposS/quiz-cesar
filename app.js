cd06350d48edcf4f1c4e55a1ad3f0d6ce186cbfa
CesarCamposS <cesarcampos.1@gmail.com>
2015-08-03 00:55:59 +0200

    Añadida la opción Borrar preguntas

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+var methodOverride = require('method-override');
 var express = require('express');
+var routes = require('./routes/index');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -6,9 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 
-var routes = require('./routes/index');
 //var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
+app.use(methodOverride('_method'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
